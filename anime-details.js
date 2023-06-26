bca6cd7d285091aeab2d4b44a6ed31abef148751
Kumbrle <86300730+Kumbrle@users.noreply.github.com>
2023-06-26 19:01:42 +0530

    Update anime-details.js

--- anime-details.js
+++ anime-details.js
@@ -1,12 +1,12 @@
 
 
 function detail(query) {
-	fetch('https://consumet-anime.up.railway.app/anime/zoro/info?id=' + query)
+	fetch('https://consumet-anime.up.railway.app/meta/anilist/info/' + query + "?provider=gogoanime")
 	.then((response) => response.json())
 	.then((animelist) => {
 		document.getElementById("img").innerHTML = "<div class='anime__details__pic set-bg' id='img1'></div>";
 		document.getElementById("img1").style.background = "url('" + animelist.image + "')";
-		document.getElementById("title").innerHTML = "<h3>" + animelist.title + "</h3>";
+		document.getElementById("title").innerHTML = "<h3>" + animelist.title[1] + "</h3>";
 		document.getElementById("description").innerHTML = animelist.description;
 		document.getElementById("type").innerHTML = "<span>Type:</span> " + animelist.type + "";
 		document.getElementById("TAN").innerHTML = "<span>Total Episodes:</span> " + animelist.totalEpisodes + "";
